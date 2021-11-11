9a91af9320bec69a9a9a7e802a502dfbeb6032b7
Jón Bjarni Ólafsson <jon.bjarni.olafsson@origo.is>
2021-11-11 23:14:31 +0000

    Fixing for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TicketsModule } from './tickets/tickets.module';
-import { MedicalCenterController } from './controllers/medical-center/medical-center.controller';
-import { ScheduleModule } from '@nestjs/schedule';
-import { MedicalCenterService } from './tickets/services/medical-center/medical-center.service';
-import { MedicalCenterModule } from './tickets/medical-center.module';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TicketsModule } from "./tickets/tickets.module";
+import { MedicalCenterController } from "./controllers/medical-center/medical-center.controller";
+import { ScheduleModule } from "@nestjs/schedule";
+import { MedicalCenterService } from "./tickets/services/medical-center/medical-center.service";
+import { MedicalCenterModule } from "./tickets/medical-center.module";
 
 @Module({
   imports: [
     TicketsModule,
     MedicalCenterModule,
     MongooseModule.forRoot(
-      'mongodb+srv://johannas:alliribidrod@bidrodincluster.sldi2.mongodb.net/ticketdb?retryWrites=true&w=majority',
+      "mongodb+srv://johannas:alliribidrod@bidrodincluster.sldi2.mongodb.net/ticketdb?retryWrites=true&w=majority"
     ),
   ],
-  controllers: [AppController, MedicalCenterModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
